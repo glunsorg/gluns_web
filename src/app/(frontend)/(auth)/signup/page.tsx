202a5ce44b107998969e5c222494fdaf2e7d92ea
geofrey254 <devongeoffreymaina@gmail.com>
2025-12-19 19:41:20 +0300

    AuthSection Renamed

--- src/app/(frontend)/(auth)/signup/page.tsx
+++ src/app/(frontend)/(auth)/signup/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import SignUp from '@/components/authentication/SignUp'
+import AuthSection from '@/components/authentication/AuthSection'
 
 export default function page() {
   return (
     <>
-      <SignUp />
+      <AuthSection />
     </>
   )
 }
